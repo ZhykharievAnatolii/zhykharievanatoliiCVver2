ab748ab3b5adea8d94cf34ffda5e77ed11033ed5
ZhykharievAnatolii <nekijt87@gmail.com>
2023-03-06 18:43:32 +0200

    fix: links to projects

--- cv/src/components/Header.js
+++ cv/src/components/Header.js
@@ -23,7 +23,7 @@ export const Header=()=>{
                                 My works
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="https://zhykharievanatolii.github.io/">Markup portfolio project</a></li>
+                                <li><a className="dropdown-item" href="https://github.com/ZhykharievAnatolii/ZhykharievAnatolii-simple-portfolio/">Markup portfolio project</a></li>
                                 <li><a className="dropdown-item" href="https://github.com/ZhykharievAnatolii/Fine-Client">Fine-Client App</a></li>
                                 <li><a className="dropdown-item" href="https://github.com/ZhykharievAnatolii/reactWorks">React works</a></li>
                                 <li><a className="dropdown-item" href="https://github.com/ZhykharievAnatolii/Client-Server-App">Client-Server App</a></li>
